7d51d97d1a71dbfdb5fe9b68b2a40e31acbe2f67
Hassan Bouzkan <bouzkanhassan@gmail.com>
2023-02-24 23:50:58 +0100

    created triangleSquare.py

---
+++ triangle.py
@@ -0,0 +1,22 @@
+import turtle
+
+# create a turtle object
+t = turtle.Turtle()
+
+# draw a triangle
+for i in range(3):
+    t.forward(100)
+    t.left(120)
+
+# move the turtle to the starting position of the square
+t.penup()
+t.goto(150, 0)
+t.pendown()
+
+# draw a square
+for i in range(4):
+    t.forward(100)
+    t.left(90)
+
+# exit the turtle screen when clicked
+turtle.exitonclick()
